Vondel-Media/vondel-server

test(compat): freeze embedded protocol behavior

internal/audiobooks/abs/ping_status_test.go
@@ -1,11 +1,171 @@
 package abs
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/Vondel-Media/vondel-server/internal/catalog"
+	"github.com/Vondel-Media/vondel-server/internal/compatcontract"
+	"github.com/Vondel-Media/vondel-server/internal/models"
 )
+
+const (
+	ordinaryProfileID = "ordinary-profile-001"
+	adultProfileID    = "adult-profile-002"
+	ordinaryItemID    = "ordinary-item-001"
+	adultItemID       = "adult-item-001"
+)
+
+type adultPolicyMediaStore struct{ noopMediaStore }
+
+func (adultPolicyMediaStore) allowed(filter catalog.AccessFilter, libraryID int) bool {
+	if filter.AllowedLibraryIDs == nil {
+		return false
+	}
+	for _, id := range filter.AllowedLibraryIDs {
+		if id == libraryID {
+			return true
+		}
+	}
+	return false
+}
+
+func (s adultPolicyMediaStore) ListAudiobookLibraries(_ context.Context, filter catalog.AccessFilter) ([]AudiobookLibrary, error) {
+	libs := []AudiobookLibrary{{ID: 1, Name: "Ordinary", Type: "audiobooks"}, {ID: 2, Name: "Adult", Type: "audiobooks"}}
+	out := make([]AudiobookLibrary, 0, len(libs))
+	for _, lib := range libs {
+		if s.allowed(filter, int(lib.ID)) {
+			out = append(out, lib)
+		}
+	}
+	return out, nil
+}
+
+func (s adultPolicyMediaStore) ListAudiobooks(_ context.Context, libraryID int64, _, _ int, filter catalog.AccessFilter, _ Filter) ([]*models.MediaItem, int, error) {
+	if !s.allowed(filter, int(libraryID)) {
+		return []*models.MediaItem{}, 0, nil
+	}
+	item := &models.MediaItem{ContentID: ordinaryItemID, Type: "audiobook", Title: "Ordinary audiobook", PosterPath: "https://images.example/ordinary.webp"}
+	if libraryID == 2 {
+		item = &models.MediaItem{ContentID: adultItemID, Type: "audiobook", Title: "Adult audiobook", PosterPath: "https://images.example/adult.webp"}
+	}
+	return []*models.MediaItem{item}, 1, nil
+}
+
+func (s adultPolicyMediaStore) GetAudiobookByID(_ context.Context, id string, filter catalog.AccessFilter) (*models.MediaItem, error) {
+	libraryID := 1
+	item := &models.MediaItem{ContentID: ordinaryItemID, Type: "audiobook", Title: "Ordinary audiobook", PosterPath: "https://images.example/ordinary.webp"}
+	if id == adultItemID {
+		libraryID = 2
+		item = &models.MediaItem{ContentID: adultItemID, Type: "audiobook", Title: "Adult audiobook", PosterPath: "https://images.example/adult.webp"}
+	}
+	if id != ordinaryItemID && id != adultItemID || !s.allowed(filter, libraryID) {
+		return nil, errors.New("not found")
+	}
+	return item, nil
+}
+
+type adultAccessResolver struct{}
+
+func (adultAccessResolver) ResolveABSAccess(_ context.Context, _ string, profileID string) (catalog.AccessFilter, error) {
+	switch profileID {
+	case ordinaryProfileID:
+		return catalog.AccessFilter{AllowedLibraryIDs: []int{1}}, nil
+	case adultProfileID:
+		return catalog.AccessFilter{AllowedLibraryIDs: []int{1, 2}}, nil
+	default:
+		return catalog.AccessFilter{AllowedLibraryIDs: []int{}}, nil
+	}
+}
+
+type fixturePublisher struct{ payloads []string }
+
+func (p *fixturePublisher) Publish(_ string, _ string, payload any) {
+	data, _ := json.Marshal(payload)
+	p.payloads = append(p.payloads, string(data))
+}
+func (p *fixturePublisher) Broadcast(_ string, payload any) {
+	data, _ := json.Marshal(payload)
+	p.payloads = append(p.payloads, string(data))
+}
+
+type fixtureProgressStore struct{}
+
+func (fixtureProgressStore) GetProgress(context.Context, string, string, string) (*ProgressRow, error) {
+	return nil, nil
+}
+func (fixtureProgressStore) ListProgressForAudiobooks(context.Context, string, string, int) ([]ProgressRow, error) {
+	return []ProgressRow{}, nil
+}
+func (fixtureProgressStore) UpsertProgress(context.Context, ProgressRow) error { return nil }
+func (fixtureProgressStore) UpdateProgressPosition(context.Context, string, string, string, float64) error {
+	return nil
+}
+func (fixtureProgressStore) SetHideFromContinue(context.Context, string, string, string, bool) error {
+	return nil
+}
+func (fixtureProgressStore) DeleteProgress(context.Context, string, string, string) error { return nil }
+
+func adultAccessToken(t *testing.T, store *memTokenStore, cfg *staticConfig, profileID string) string {
+	t.Helper()
+	jti := "fixture-" + profileID
+	token, err := IssueAccessToken(cfg.secret, "fixture-user", profileID, jti, time.Hour)
+	if err != nil {
+		t.Fatalf("IssueAccessToken: %v", err)
+	}
+	if err := store.InsertToken(context.Background(), ABSToken{ID: jti, UserID: "fixture-user", ProfileID: profileID, JTI: jti, ExpiresAt: time.Now().Add(time.Hour)}); err != nil {
+		t.Fatalf("InsertToken: %v", err)
+	}
+	return token
+}
+
+func TestEmbeddedAudiobookshelfAdultPolicyContract(t *testing.T) {
+	store := newMemTokenStore()
+	cfg := &staticConfig{secret: []byte("test-secret-32-bytes-aaaaaaaaaaaaa")}
+	publisher := &fixturePublisher{}
+	h := New(Dependencies{Config: cfg, TokenStore: store, MediaStore: adultPolicyMediaStore{}, AccessResolver: adultAccessResolver{}, Publisher: publisher, ProgressStore: fixtureProgressStore{}})
+	server := httptest.NewServer(h.Router())
+	defer server.Close()
+	ordinary := adultAccessToken(t, store, cfg, ordinaryProfileID)
+	adult := adultAccessToken(t, store, cfg, adultProfileID)
+
+	run := func(name, token string, suite compatcontract.Suite) {
+		t.Helper()
+		report, err := compatcontract.Run(context.Background(), compatcontract.Target{BaseURL: server.URL, Client: server.Client(), Credentials: compatcontract.CredentialFunc(func(req *http.Request) error { req.Header.Set("Authorization", "Bearer "+token); return nil })}, suite)
+		if err != nil {
+			t.Fatalf("%s: %v; report=%s", name, err, report.JSON())
+		}
+	}
+	run("ordinary", ordinary, compatcontract.AudiobookshelfOrdinaryAdultPolicy())
+	if len(publisher.payloads) != 0 {
+		t.Fatalf("ordinary profile activity leaked: %v", publisher.payloads)
+	}
+	run("adult", adult, compatcontract.AudiobookshelfAuthorizedAdultPolicy())
+}
+
+func TestEmbeddedAudiobookshelfCompatibilityContract(t *testing.T) {
+	h := New(Dependencies{
+		Config:        &staticConfig{secret: []byte("test-secret-32-bytes-aaaaaaaaaaaaa")},
+		TokenStore:    newMemTokenStore(),
+		CredValidator: &recordingValidator{},
+		MediaStore:    noopMediaStore{},
+	})
+	server := httptest.NewServer(h.Router())
+	defer server.Close()
+
+	report, err := compatcontract.Run(context.Background(), compatcontract.Target{
+		BaseURL: server.URL,
+		Client:  server.Client(),
+	}, compatcontract.AudiobookshelfBaseline())
+	if err != nil {
+		t.Fatalf("embedded Audiobookshelf compatibility contract: %v; report=%s", err, report.JSON())
+	}
+}
 
 // TestABSPing_HasSuccess guards the reachability probe: real ABS /ping returns
 // {"success": true} and the ABS apps validate a server address by reading that

internal/jellycompat/router_test.go
@@ -2,16 +2,163 @@ package jellycompat
 
 import (
 	"compress/gzip"
+	"context"
+	"errors"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/Vondel-Media/vondel-server/internal/catalog"
+	"github.com/Vondel-Media/vondel-server/internal/compatcontract"
 	"github.com/Vondel-Media/vondel-server/internal/config"
 )
+
+const (
+	fixtureOrdinaryProfile = "ordinary-profile-001"
+	fixtureAdultProfile    = "adult-profile-002"
+	fixtureOrdinaryItem    = "ordinary-item-001"
+	fixtureAdultItem       = "adult-item-001"
+)
+
+type fixturePolicyContent struct{ access AccessFilterResolver }
+
+func (c fixturePolicyContent) visible(session *Session, libraryID int) bool {
+	filter := c.access(context.Background(), session.StreamAppUserID, session.ProfileID)
+	for _, id := range filter.AllowedLibraryIDs {
+		if id == libraryID {
+			return true
+		}
+	}
+	return false
+}
+func (c fixturePolicyContent) ListUserLibraries(_ context.Context, s *Session) ([]upstreamUserLibrary, error) {
+	libs := []upstreamUserLibrary{{ID: 1, Name: "Ordinary", Type: "movies"}, {ID: 2, Name: "Adult", Type: "movies"}}
+	out := []upstreamUserLibrary{}
+	for _, lib := range libs {
+		if c.visible(s, lib.ID) {
+			out = append(out, lib)
+		}
+	}
+	return out, nil
+}
+func (c fixturePolicyContent) BrowseItems(_ context.Context, s *Session, _ url.Values) (*upstreamBrowseResponse, error) {
+	items := []upstreamListItem{{ContentID: fixtureOrdinaryItem, Type: "movie", Title: "Ordinary title"}}
+	if c.visible(s, 2) {
+		items = append(items, upstreamListItem{ContentID: fixtureAdultItem, Type: "movie", Title: "Adult title"})
+	}
+	return &upstreamBrowseResponse{Items: items, Total: len(items)}, nil
+}
+func (c fixturePolicyContent) SearchItems(ctx context.Context, s *Session, _ SearchItemsOptions) (*upstreamBrowseResponse, error) {
+	return c.BrowseItems(ctx, s, nil)
+}
+func (c fixturePolicyContent) GetItemDetail(_ context.Context, s *Session, id string, _ *int) (*upstreamItemDetail, error) {
+	if id == fixtureAdultItem && !c.visible(s, 2) {
+		return nil, &HTTPError{StatusCode: http.StatusNotFound, Message: "not found"}
+	}
+	if id != fixtureOrdinaryItem && id != fixtureAdultItem {
+		return nil, &HTTPError{StatusCode: http.StatusNotFound, Message: "not found"}
+	}
+	title := "Ordinary title"
+	if id == fixtureAdultItem {
+		title = "Adult title"
+	}
+	return &upstreamItemDetail{ContentID: id, Type: "movie", Title: title}, nil
+}
+func (c fixturePolicyContent) GetItemDetailsByIDs(ctx context.Context, s *Session, ids []string, l *int) (map[string]*upstreamItemDetail, error) {
+	out := map[string]*upstreamItemDetail{}
+	for _, id := range ids {
+		if item, err := c.GetItemDetail(ctx, s, id, l); err == nil {
+			out[id] = item
+		}
+	}
+	return out, nil
+}
+func (fixturePolicyContent) ListSeasons(context.Context, *Session, string, *int) ([]upstreamSeason, error) {
+	return []upstreamSeason{}, nil
+}
+func (fixturePolicyContent) GetSeason(context.Context, *Session, string, int, *int) (*upstreamSeason, error) {
+	return nil, errors.New("not found")
+}
+func (fixturePolicyContent) ListEpisodes(context.Context, *Session, string, int, *int) ([]upstreamEpisode, error) {
+	return []upstreamEpisode{}, nil
+}
+func (fixturePolicyContent) ListEpisodesBySeasonID(context.Context, *Session, string, *int) ([]upstreamEpisode, error) {
+	return []upstreamEpisode{}, nil
+}
+func (fixturePolicyContent) ListItemFilters(context.Context, *Session, url.Values) (*upstreamItemFiltersResponse, error) {
+	return &upstreamItemFiltersResponse{}, nil
+}
+func (fixturePolicyContent) EnrichSeriesUserData(context.Context, *Session, []upstreamListItem) {}
+
+func fixtureAccessFilter(_ context.Context, _ int, profileID string) catalog.AccessFilter {
+	if profileID == fixtureAdultProfile {
+		return catalog.AccessFilter{AllowedLibraryIDs: []int{1, 2}}
+	}
+	return catalog.AccessFilter{AllowedLibraryIDs: []int{1}}
+}
+
+func TestEmbeddedJellyfinAdultPolicyContract(t *testing.T) {
+	cfg, err := config.LoadFromDB(map[string]string{})
+	if err != nil {
+		t.Fatalf("LoadFromDB: %v", err)
+	}
+	codec := NewResourceIDCodec()
+	store := NewSessionStore(time.Hour, time.Now)
+	for _, profile := range []string{fixtureOrdinaryProfile, fixtureAdultProfile} {
+		token := "fixture-" + profile
+		if err := store.Put(Session{Token: token, StreamAppUserID: 7, ProfileID: profile, PseudoUserID: PseudoUserID(7, profile)}); err != nil {
+			t.Fatalf("put %s session: %v", profile, err)
+		}
+	}
+	content := fixturePolicyContent{access: fixtureAccessFilter}
+	server := httptest.NewServer(NewRouter(Dependencies{Config: cfg, IDCodec: codec, SessionStore: store, Authenticator: NewAuthenticator(store, nil), ContentService: content, UserDataService: &mockUserDataService{}, AccessFilterFn: fixtureAccessFilter}))
+	defer server.Close()
+	run := func(name, profile string, suite compatcontract.Suite) {
+		t.Helper()
+		itemID := codec.EncodeStringID(EncodedIDItem, fixtureAdultItem)
+		userID := PseudoUserID(7, profile).String()
+		for i := range suite.Cases {
+			suite.Cases[i].Path = strings.ReplaceAll(suite.Cases[i].Path, "adult-item-001", itemID)
+			suite.Cases[i].Path = strings.ReplaceAll(suite.Cases[i].Path, "random-missing-002", codec.EncodeStringID(EncodedIDItem, "random-missing-002"))
+			suite.Cases[i].Path = strings.ReplaceAll(suite.Cases[i].Path, "/Users/fixture/", "/Users/"+userID+"/")
+			if profile == fixtureOrdinaryProfile {
+				suite.Cases[i].AbsentStrings = append(suite.Cases[i].AbsentStrings, itemID)
+			}
+		}
+		report, err := compatcontract.Run(context.Background(), compatcontract.Target{BaseURL: server.URL, Client: server.Client(), Credentials: compatcontract.CredentialFunc(func(req *http.Request) error { req.Header.Set("X-Emby-Token", "fixture-"+profile); return nil })}, suite)
+		if err != nil {
+			t.Fatalf("%s: %v; report=%s", name, err, report.JSON())
+		}
+	}
+	run("ordinary", fixtureOrdinaryProfile, compatcontract.JellyfinOrdinaryAdultPolicy())
+	run("adult", fixtureAdultProfile, compatcontract.JellyfinAuthorizedAdultPolicy())
+}
+
+func TestEmbeddedJellyfinCompatibilityContract(t *testing.T) {
+	cfg, err := config.LoadFromDB(map[string]string{})
+	if err != nil {
+		t.Fatalf("LoadFromDB: %v", err)
+	}
+	server := httptest.NewServer(NewRouter(Dependencies{
+		Config:        cfg,
+		Authenticator: NewAuthenticator(NewSessionStore(time.Hour, time.Now), nil),
+	}))
+	defer server.Close()
+
+	report, err := compatcontract.Run(context.Background(), compatcontract.Target{
+		BaseURL: server.URL,
+		Client:  server.Client(),
+	}, compatcontract.JellyfinBaseline())
+	if err != nil {
+		t.Fatalf("embedded Jellyfin compatibility contract: %v; report=%s", err, report.JSON())
+	}
+}
 
 func TestRouterCompressesJSONResponses(t *testing.T) {
 	cfg, err := config.LoadFromDB(map[string]string{})

Bloem-Studios/bloem-server

Cherry-picked from feature/compatibility-sidecars rather than merged. That
branch is built on the hard-fork history, so merging it would have pulled the
zero-parent import back into main and undone the restoration — the branch's one
commit of real work, 1,103 lines, is what was wanted.
Its import paths are rewritten onto upstream's module path, which was also the
only conflict: the same import block, once as Silo-Server and once as
Vondel-Media.
Builds, vets, and internal/compatcontract and internal/jellycompat both pass.

internal/audiobooks/abs/ping_status_test.go
@@ -1,11 +1,171 @@
 package abs
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/Silo-Server/silo-server/internal/catalog"
+	"github.com/Silo-Server/silo-server/internal/compatcontract"
+	"github.com/Silo-Server/silo-server/internal/models"
 )
+
+const (
+	ordinaryProfileID = "ordinary-profile-001"
+	adultProfileID    = "adult-profile-002"
+	ordinaryItemID    = "ordinary-item-001"
+	adultItemID       = "adult-item-001"
+)
+
+type adultPolicyMediaStore struct{ noopMediaStore }
+
+func (adultPolicyMediaStore) allowed(filter catalog.AccessFilter, libraryID int) bool {
+	if filter.AllowedLibraryIDs == nil {
+		return false
+	}
+	for _, id := range filter.AllowedLibraryIDs {
+		if id == libraryID {
+			return true
+		}
+	}
+	return false
+}
+
+func (s adultPolicyMediaStore) ListAudiobookLibraries(_ context.Context, filter catalog.AccessFilter) ([]AudiobookLibrary, error) {
+	libs := []AudiobookLibrary{{ID: 1, Name: "Ordinary", Type: "audiobooks"}, {ID: 2, Name: "Adult", Type: "audiobooks"}}
+	out := make([]AudiobookLibrary, 0, len(libs))
+	for _, lib := range libs {
+		if s.allowed(filter, int(lib.ID)) {
+			out = append(out, lib)
+		}
+	}
+	return out, nil
+}
+
+func (s adultPolicyMediaStore) ListAudiobooks(_ context.Context, libraryID int64, _, _ int, filter catalog.AccessFilter, _ Filter) ([]*models.MediaItem, int, error) {
+	if !s.allowed(filter, int(libraryID)) {
+		return []*models.MediaItem{}, 0, nil
+	}
+	item := &models.MediaItem{ContentID: ordinaryItemID, Type: "audiobook", Title: "Ordinary audiobook", PosterPath: "https://images.example/ordinary.webp"}
+	if libraryID == 2 {
+		item = &models.MediaItem{ContentID: adultItemID, Type: "audiobook", Title: "Adult audiobook", PosterPath: "https://images.example/adult.webp"}
+	}
+	return []*models.MediaItem{item}, 1, nil
+}
+
+func (s adultPolicyMediaStore) GetAudiobookByID(_ context.Context, id string, filter catalog.AccessFilter) (*models.MediaItem, error) {
+	libraryID := 1
+	item := &models.MediaItem{ContentID: ordinaryItemID, Type: "audiobook", Title: "Ordinary audiobook", PosterPath: "https://images.example/ordinary.webp"}
+	if id == adultItemID {
+		libraryID = 2
+		item = &models.MediaItem{ContentID: adultItemID, Type: "audiobook", Title: "Adult audiobook", PosterPath: "https://images.example/adult.webp"}
+	}
+	if id != ordinaryItemID && id != adultItemID || !s.allowed(filter, libraryID) {
+		return nil, errors.New("not found")
+	}
+	return item, nil
+}
+
+type adultAccessResolver struct{}
+
+func (adultAccessResolver) ResolveABSAccess(_ context.Context, _ string, profileID string) (catalog.AccessFilter, error) {
+	switch profileID {
+	case ordinaryProfileID:
+		return catalog.AccessFilter{AllowedLibraryIDs: []int{1}}, nil
+	case adultProfileID:
+		return catalog.AccessFilter{AllowedLibraryIDs: []int{1, 2}}, nil
+	default:
+		return catalog.AccessFilter{AllowedLibraryIDs: []int{}}, nil
+	}
+}
+
+type fixturePublisher struct{ payloads []string }
+
+func (p *fixturePublisher) Publish(_ string, _ string, payload any) {
+	data, _ := json.Marshal(payload)
+	p.payloads = append(p.payloads, string(data))
+}
+func (p *fixturePublisher) Broadcast(_ string, payload any) {
+	data, _ := json.Marshal(payload)
+	p.payloads = append(p.payloads, string(data))
+}
+
+type fixtureProgressStore struct{}
+
+func (fixtureProgressStore) GetProgress(context.Context, string, string, string) (*ProgressRow, error) {
+	return nil, nil
+}
+func (fixtureProgressStore) ListProgressForAudiobooks(context.Context, string, string, int) ([]ProgressRow, error) {
+	return []ProgressRow{}, nil
+}
+func (fixtureProgressStore) UpsertProgress(context.Context, ProgressRow) error { return nil }
+func (fixtureProgressStore) UpdateProgressPosition(context.Context, string, string, string, float64) error {
+	return nil
+}
+func (fixtureProgressStore) SetHideFromContinue(context.Context, string, string, string, bool) error {
+	return nil
+}
+func (fixtureProgressStore) DeleteProgress(context.Context, string, string, string) error { return nil }
+
+func adultAccessToken(t *testing.T, store *memTokenStore, cfg *staticConfig, profileID string) string {
+	t.Helper()
+	jti := "fixture-" + profileID
+	token, err := IssueAccessToken(cfg.secret, "fixture-user", profileID, jti, time.Hour)
+	if err != nil {
+		t.Fatalf("IssueAccessToken: %v", err)
+	}
+	if err := store.InsertToken(context.Background(), ABSToken{ID: jti, UserID: "fixture-user", ProfileID: profileID, JTI: jti, ExpiresAt: time.Now().Add(time.Hour)}); err != nil {
+		t.Fatalf("InsertToken: %v", err)
+	}
+	return token
+}
+
+func TestEmbeddedAudiobookshelfAdultPolicyContract(t *testing.T) {
+	store := newMemTokenStore()
+	cfg := &staticConfig{secret: []byte("test-secret-32-bytes-aaaaaaaaaaaaa")}
+	publisher := &fixturePublisher{}
+	h := New(Dependencies{Config: cfg, TokenStore: store, MediaStore: adultPolicyMediaStore{}, AccessResolver: adultAccessResolver{}, Publisher: publisher, ProgressStore: fixtureProgressStore{}})
+	server := httptest.NewServer(h.Router())
+	defer server.Close()
+	ordinary := adultAccessToken(t, store, cfg, ordinaryProfileID)
+	adult := adultAccessToken(t, store, cfg, adultProfileID)
+
+	run := func(name, token string, suite compatcontract.Suite) {
+		t.Helper()
+		report, err := compatcontract.Run(context.Background(), compatcontract.Target{BaseURL: server.URL, Client: server.Client(), Credentials: compatcontract.CredentialFunc(func(req *http.Request) error { req.Header.Set("Authorization", "Bearer "+token); return nil })}, suite)
+		if err != nil {
+			t.Fatalf("%s: %v; report=%s", name, err, report.JSON())
+		}
+	}
+	run("ordinary", ordinary, compatcontract.AudiobookshelfOrdinaryAdultPolicy())
+	if len(publisher.payloads) != 0 {
+		t.Fatalf("ordinary profile activity leaked: %v", publisher.payloads)
+	}
+	run("adult", adult, compatcontract.AudiobookshelfAuthorizedAdultPolicy())
+}
+
+func TestEmbeddedAudiobookshelfCompatibilityContract(t *testing.T) {
+	h := New(Dependencies{
+		Config:        &staticConfig{secret: []byte("test-secret-32-bytes-aaaaaaaaaaaaa")},
+		TokenStore:    newMemTokenStore(),
+		CredValidator: &recordingValidator{},
+		MediaStore:    noopMediaStore{},
+	})
+	server := httptest.NewServer(h.Router())
+	defer server.Close()
+
+	report, err := compatcontract.Run(context.Background(), compatcontract.Target{
+		BaseURL: server.URL,
+		Client:  server.Client(),
+	}, compatcontract.AudiobookshelfBaseline())
+	if err != nil {
+		t.Fatalf("embedded Audiobookshelf compatibility contract: %v; report=%s", err, report.JSON())
+	}
+}
 
 // TestABSPing_HasSuccess guards the reachability probe: real ABS /ping returns
 // {"success": true} and the ABS apps validate a server address by reading that

internal/jellycompat/router_test.go
@@ -2,16 +2,163 @@ package jellycompat
 
 import (
 	"compress/gzip"
+	"context"
+	"errors"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/Silo-Server/silo-server/internal/catalog"
+	"github.com/Silo-Server/silo-server/internal/compatcontract"
 	"github.com/Silo-Server/silo-server/internal/config"
 )
+
+const (
+	fixtureOrdinaryProfile = "ordinary-profile-001"
+	fixtureAdultProfile    = "adult-profile-002"
+	fixtureOrdinaryItem    = "ordinary-item-001"
+	fixtureAdultItem       = "adult-item-001"
+)
+
+type fixturePolicyContent struct{ access AccessFilterResolver }
+
+func (c fixturePolicyContent) visible(session *Session, libraryID int) bool {
+	filter := c.access(context.Background(), session.StreamAppUserID, session.ProfileID)
+	for _, id := range filter.AllowedLibraryIDs {
+		if id == libraryID {
+			return true
+		}
+	}
+	return false
+}
+func (c fixturePolicyContent) ListUserLibraries(_ context.Context, s *Session) ([]upstreamUserLibrary, error) {
+	libs := []upstreamUserLibrary{{ID: 1, Name: "Ordinary", Type: "movies"}, {ID: 2, Name: "Adult", Type: "movies"}}
+	out := []upstreamUserLibrary{}
+	for _, lib := range libs {
+		if c.visible(s, lib.ID) {
+			out = append(out, lib)
+		}
+	}
+	return out, nil
+}
+func (c fixturePolicyContent) BrowseItems(_ context.Context, s *Session, _ url.Values) (*upstreamBrowseResponse, error) {
+	items := []upstreamListItem{{ContentID: fixtureOrdinaryItem, Type: "movie", Title: "Ordinary title"}}
+	if c.visible(s, 2) {
+		items = append(items, upstreamListItem{ContentID: fixtureAdultItem, Type: "movie", Title: "Adult title"})
+	}
+	return &upstreamBrowseResponse{Items: items, Total: len(items)}, nil
+}
+func (c fixturePolicyContent) SearchItems(ctx context.Context, s *Session, _ SearchItemsOptions) (*upstreamBrowseResponse, error) {
+	return c.BrowseItems(ctx, s, nil)
+}
+func (c fixturePolicyContent) GetItemDetail(_ context.Context, s *Session, id string, _ *int) (*upstreamItemDetail, error) {
+	if id == fixtureAdultItem && !c.visible(s, 2) {
+		return nil, &HTTPError{StatusCode: http.StatusNotFound, Message: "not found"}
+	}
+	if id != fixtureOrdinaryItem && id != fixtureAdultItem {
+		return nil, &HTTPError{StatusCode: http.StatusNotFound, Message: "not found"}
+	}
+	title := "Ordinary title"
+	if id == fixtureAdultItem {
+		title = "Adult title"
+	}
+	return &upstreamItemDetail{ContentID: id, Type: "movie", Title: title}, nil
+}
+func (c fixturePolicyContent) GetItemDetailsByIDs(ctx context.Context, s *Session, ids []string, l *int) (map[string]*upstreamItemDetail, error) {
+	out := map[string]*upstreamItemDetail{}
+	for _, id := range ids {
+		if item, err := c.GetItemDetail(ctx, s, id, l); err == nil {
+			out[id] = item
+		}
+	}
+	return out, nil
+}
+func (fixturePolicyContent) ListSeasons(context.Context, *Session, string, *int) ([]upstreamSeason, error) {
+	return []upstreamSeason{}, nil
+}
+func (fixturePolicyContent) GetSeason(context.Context, *Session, string, int, *int) (*upstreamSeason, error) {
+	return nil, errors.New("not found")
+}
+func (fixturePolicyContent) ListEpisodes(context.Context, *Session, string, int, *int) ([]upstreamEpisode, error) {
+	return []upstreamEpisode{}, nil
+}
+func (fixturePolicyContent) ListEpisodesBySeasonID(context.Context, *Session, string, *int) ([]upstreamEpisode, error) {
+	return []upstreamEpisode{}, nil
+}
+func (fixturePolicyContent) ListItemFilters(context.Context, *Session, url.Values) (*upstreamItemFiltersResponse, error) {
+	return &upstreamItemFiltersResponse{}, nil
+}
+func (fixturePolicyContent) EnrichSeriesUserData(context.Context, *Session, []upstreamListItem) {}
+
+func fixtureAccessFilter(_ context.Context, _ int, profileID string) catalog.AccessFilter {
+	if profileID == fixtureAdultProfile {
+		return catalog.AccessFilter{AllowedLibraryIDs: []int{1, 2}}
+	}
+	return catalog.AccessFilter{AllowedLibraryIDs: []int{1}}
+}
+
+func TestEmbeddedJellyfinAdultPolicyContract(t *testing.T) {
+	cfg, err := config.LoadFromDB(map[string]string{})
+	if err != nil {
+		t.Fatalf("LoadFromDB: %v", err)
+	}
+	codec := NewResourceIDCodec()
+	store := NewSessionStore(time.Hour, time.Now)
+	for _, profile := range []string{fixtureOrdinaryProfile, fixtureAdultProfile} {
+		token := "fixture-" + profile
+		if err := store.Put(Session{Token: token, StreamAppUserID: 7, ProfileID: profile, PseudoUserID: PseudoUserID(7, profile)}); err != nil {
+			t.Fatalf("put %s session: %v", profile, err)
+		}
+	}
+	content := fixturePolicyContent{access: fixtureAccessFilter}
+	server := httptest.NewServer(NewRouter(Dependencies{Config: cfg, IDCodec: codec, SessionStore: store, Authenticator: NewAuthenticator(store, nil), ContentService: content, UserDataService: &mockUserDataService{}, AccessFilterFn: fixtureAccessFilter}))
+	defer server.Close()
+	run := func(name, profile string, suite compatcontract.Suite) {
+		t.Helper()
+		itemID := codec.EncodeStringID(EncodedIDItem, fixtureAdultItem)
+		userID := PseudoUserID(7, profile).String()
+		for i := range suite.Cases {
+			suite.Cases[i].Path = strings.ReplaceAll(suite.Cases[i].Path, "adult-item-001", itemID)
+			suite.Cases[i].Path = strings.ReplaceAll(suite.Cases[i].Path, "random-missing-002", codec.EncodeStringID(EncodedIDItem, "random-missing-002"))
+			suite.Cases[i].Path = strings.ReplaceAll(suite.Cases[i].Path, "/Users/fixture/", "/Users/"+userID+"/")
+			if profile == fixtureOrdinaryProfile {
+				suite.Cases[i].AbsentStrings = append(suite.Cases[i].AbsentStrings, itemID)
+			}
+		}
+		report, err := compatcontract.Run(context.Background(), compatcontract.Target{BaseURL: server.URL, Client: server.Client(), Credentials: compatcontract.CredentialFunc(func(req *http.Request) error { req.Header.Set("X-Emby-Token", "fixture-"+profile); return nil })}, suite)
+		if err != nil {
+			t.Fatalf("%s: %v; report=%s", name, err, report.JSON())
+		}
+	}
+	run("ordinary", fixtureOrdinaryProfile, compatcontract.JellyfinOrdinaryAdultPolicy())
+	run("adult", fixtureAdultProfile, compatcontract.JellyfinAuthorizedAdultPolicy())
+}
+
+func TestEmbeddedJellyfinCompatibilityContract(t *testing.T) {
+	cfg, err := config.LoadFromDB(map[string]string{})
+	if err != nil {
+		t.Fatalf("LoadFromDB: %v", err)
+	}
+	server := httptest.NewServer(NewRouter(Dependencies{
+		Config:        cfg,
+		Authenticator: NewAuthenticator(NewSessionStore(time.Hour, time.Now), nil),
+	}))
+	defer server.Close()
+
+	report, err := compatcontract.Run(context.Background(), compatcontract.Target{
+		BaseURL: server.URL,
+		Client:  server.Client(),
+	}, compatcontract.JellyfinBaseline())
+	if err != nil {
+		t.Fatalf("embedded Jellyfin compatibility contract: %v; report=%s", err, report.JSON())
+	}
+}
 
 func TestRouterCompressesJSONResponses(t *testing.T) {
 	cfg, err := config.LoadFromDB(map[string]string{})